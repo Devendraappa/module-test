2c2ae42e9a5394360c96e8130adfc0b1fdfdb22e
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 13:08:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "https://github.com/Devendraappa/pvt-repo-module"
+  source = "https://github.com/Devendraappa/pvt-repo-module.git"
   
   # Add any input variables for the module here
   region = "ap-south-1"  # Example of passing a variable to the module
