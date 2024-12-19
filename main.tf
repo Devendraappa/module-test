6dd3e59a512e3c8c4e8df254c4ff9a45018cf6c7
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 13:52:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ provider "aws" {
 
 module "vpc" {
   source = "git::github.com/devendraappa/pvt-repo-module//?ref=v1.0.0"
-  token  = var.git_token
 
   region              = var.region
   vpc_cidr            = var.vpc_cidr
@@ -21,10 +20,39 @@ module "vpc" {
   vpc_name            = var.vpc_name
 }
 
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
 output "vpc_name" {
   value = module.vpc.vpc_name
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
 }
