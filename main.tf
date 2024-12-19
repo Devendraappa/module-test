b0a98c38486c6424abe8602c9f0bb01411df9144
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 12:29:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "git::https://github.com/devendraappa/pvt-repo-module.git//vpc-module"
+  source = "git::https://github.com/devendraappa/pvt-repo-module.git//vpc-module?ref=main"
 
   region              = "ap-south-1"
   vpc_cidr            = "10.0.0.0/16"
