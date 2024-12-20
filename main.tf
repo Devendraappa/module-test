ea698d83d42db7e3d6b2b0d8bcf4e5ea6fe61810
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 12:41:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,5 @@
-#module "vpc" {
-  #source = "git::github.com/devendraappa/pvt-repo-module//?ref=v1.0.0"
-  # https://github.com/Devendraappa/pvt-repo-module.git
-  
-  # Add any input variables for the module here
-  #region = "ap-south-1"  # Example of passing a variable to the module
-#}
-
-#provider "aws" {
-#  region = "ap-south-1"
-#}
-
 module "vpc" {
-  source = "git::https://github.com/Devendraappa/pvt-repo-module.git
-
-  #region              = "ap-south-1"
-  #vpc_cidr            = "10.0.0.0/16"
-  #public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-  #availability_zones  = ["ap-south-1a", "ap-south-1b"]
-  #vpc_name            = "module-vpc"
+  source  = "git::https://github.com/Devendraappa/pvt-repo-module.git?ref=v1.0.0"
+  cidr_block = "10.0.0.0/16"
+  name       = "my-vpc"
 }
