219ffdb850854abbe9d46ca17c35a28db4ba3add
Devendraappa <devendraappa.kune@gmail.com>
2024-12-20 13:01:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source  = "git::https://github.com/Devendraappa/pvt-repo-module.git?ref=v1.0.0"
+  source     = "git::https://github.com/Devendraappa/pvt-repo-module.git?ref=v1.0.0"
   cidr_block = "10.0.0.0/16"
   name       = "my-vpc"
 }
